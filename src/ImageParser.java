0b4e4c8e5439d6cb0eb7db0a4e28f01b936486ee
Glebs <jjb15133@strath.ac.uk>
2018-10-06 18:17:04 +0100

    Updated version

--- src/ImageParser.java
+++ src/ImageParser.java
@@ -2,8 +2,10 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.math.BigInteger;
+import java.util.concurrent.TimeUnit;
 
 public class ImageParser {
+    private final int messageLength = 83;
     private BufferedImage img = null;
     private String stringRepresent = "";
 
@@ -31,16 +33,24 @@ public class ImageParser {
         int w = img.getWidth();
         int h = img.getHeight();
 
+        long startTime = System.currentTimeMillis();
+        System.out.println("started processing...");
+
         for (int i = h - 1; i >= 0; i--) {
             for (int j = w - 1; j >= 0; j--) {
                 int pixel = img.getRGB(j, i);
-                System.out.println("x:" + j + "y:" + i);
-                extractAllZeroBits(pixel);
+                extractZeroBits(pixel);
             }
         }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("finished processing. Time it took: ");
+        System.out.print(TimeUnit.MILLISECONDS.toSeconds(endTime - startTime));
+        System.out.print(" seconds");
+        System.out.println();
     }
 
-    private void extractAllZeroBits(int pixel) {
+    private void extractZeroBits(int pixel) {
         String zeroBits = "";
         zeroBits += (pixel >> 16) & 0x1;
         zeroBits += (pixel >> 8) & 0x1;
@@ -68,6 +78,34 @@ public class ImageParser {
         }
     }
 
+    private void findMatch(byte[] bytes) {
+        for (int i = 0; i < bytes.length; i++) {
+            if(isMatch(bytes, i)) {
+                System.out.println("Found match");
+                extractArtefact(bytes, i);
+                return;
+            }
+        }
+    }
+
+    private void extractArtefact(byte[] bytes, int i) {
+        byte[] message = new byte[messageLength];
+        for (int j = 1; j <= message.length ; j++) {
+            message[j-1] = bytes[i+messageLength-j];
+        }
+        writeToFile(message);
+    }
+
+    private boolean isMatch(byte[] bytes, int index) {
+        for (int j = 0; j < messageLength; j++) {
+            int symbol = bytes[index+j] & 0xFF;
+            if(!(symbol >= 32 && symbol <= 122)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     private String getStringRepresent() {
         return stringRepresent;
     }
@@ -76,6 +114,6 @@ public class ImageParser {
         ImageParser parser = new ImageParser();
         parser.loadImage();
         parser.extractPixels();
-        parser.writeToFile(new BigInteger(parser.getStringRepresent(), 2).toByteArray());
+        parser.findMatch(new BigInteger(parser.getStringRepresent(), 2).toByteArray());
     }
 }
